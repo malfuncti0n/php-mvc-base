c7f68812b489195b2f0befd2ff696c965bbf132e
malfuncti0n <savvaniss@yahoo.gr>
2016-09-12 20:22:06 +0300

    Update App.php
    with new config

--- app/core/App.php
+++ app/core/App.php
@@ -5,24 +5,49 @@
  * Date: 9/11/2016
  * Time: 7:43 PM
  */
+
+
+use Noodlehaus\Config;
+
 class App{
 
+  // protected $config;
 
     protected $controller='homeController';
+    //arkikopoieis wste panta na fortonei mode, akoma kai otan de tou dosoume
+    protected $mode;
 
     protected $method='index';
 
     protected $params=[];
 
+
+
+
     public function __construct()
     {
+
         $url=[];
         //to url poy exei ginei rewrite apo to .htacces se metavlitikh get
         //to spame kai to epistrefoume se array
-      $url= $this->parseUrl();
+       $url= $this->parseUrl();
 
-        //an h prwti parametros poy kaname extrack yparxei
-        //kane require ton controller
+        //pernoume to mode poy exei dilwthei sro arxzei
+        $this->mode=file_get_contents('../mode.php');
+
+
+        //pernaw to config se mia metavliti diathesimh se olo to contex
+        global $config;
+        $config=Config::load('../app/config/'. $this->mode .'.php');
+
+
+
+        //database connection
+        require_once '../app/database.php';
+        //telos database connection
+
+
+        //apo edw kai kato controller kai methods
         if(file_exists('../app/controllers/'. $url[0]. 'Controller.php')){
             $this->controller = $url[0].'Controller';
             unset($url[0]);
@@ -39,6 +64,7 @@ class App{
             }
 
         }
+
         //oti einai meta th methodo to epistrefoume san array
         //an einai keno, epistrefoume adio array
         if(isset($url)) {
@@ -57,5 +83,9 @@ class App{
           return $url=explode('/',filter_var(rtrim($_GET['url'], '/'),FILTER_SANITIZE_URL));
 
         }
+    }
+
+    public function getMode(){
+
     }
 }
