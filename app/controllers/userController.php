ad5011cf3a63c91263eb956b067c3de9cbb62e1e
malfuncti0n <savvaniss@yahoo.gr>
2016-09-12 13:33:01 +0300

    flash msg changes
    flash msg changes

--- app/controllers/userController.php
+++ app/controllers/userController.php
@@ -11,7 +11,7 @@ class userController extends Controller
     //flash message implementation
     
     public function index($params=[]){
-        $params=['msg'=>'Render is ok '];
+       // $params=['msg'=>'Render is ok '];
        // $this->view->()->addGobal('params',$params);
         echo $this->view()->render('user/index.twig.html',array('params'=>$params));
        
